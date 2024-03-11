8c6d6131dd7db72aa2909e2b37210b6c836e168c
SeoU <tjdnfkd1225@naver.com>
2024-03-11 17:31:46 +0900

    feat: TabBarController 및 HomeVC, LaunchScreen 기본 설정

---
+++ Allergykimi/Components/CustomTabBarController.swift
@@ -0,0 +1,39 @@
+//
+//  CustomTabBarController.swift
+//  Allergykimi
+//
+//  Created by 은서우 on 3/11/24.
+//
+
+import UIKit
+
+class CustomTabBarController: UITabBarController {
+    
+    let homeNavController = UINavigationController(rootViewController: HomeViewController())
+    let mapNavController = UINavigationController(rootViewController: ViewController())
+    let myPageNavController = UINavigationController(rootViewController: ViewController())
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 0 : 맵뷰
+        let mapTabBarItem = UITabBarItem(title: nil, image:  UIImage(systemName: "pill"), selectedImage: UIImage(systemName: "pill.fill"))
+        mapTabBarItem.tag = 0
+        mapNavController.tabBarItem = mapTabBarItem
+        // 1 : 홈뷰
+        let homeTabBarItem = UITabBarItem(title: nil, image:  UIImage(systemName: "house"), selectedImage: UIImage(systemName: "house.fill"))
+        homeTabBarItem.tag = 1
+        homeNavController.tabBarItem = homeTabBarItem
+        // 2 : 마이페이지 뷰
+        let myPageTabBarItem = UITabBarItem(title: nil, image:  UIImage(systemName: "person"), selectedImage: UIImage(systemName: "person.fill"))
+        myPageTabBarItem.tag = 2
+        myPageNavController.tabBarItem = myPageTabBarItem
+        
+        self.viewControllers = [mapNavController, homeNavController, myPageNavController]
+        
+    }
+    
+    
+    
+    
+}
---
+++ Allergykimi/Home/HomeViewController.swift
@@ -0,0 +1,31 @@
+//
+//  HomeViewController.swift
+//  Allergykimi
+//
+//  Created by 은서우 on 3/11/24.
+//
+
+import UIKit
+
+class HomeViewController: BaseViewController {
+
+    let searchBar = UISearchBar()
+    
+    override func configureHierarchy() {
+        view.addSubview(searchBar)
+    }
+    
+    override func configureView() {
+        super.configureView()
+        
+        searchBar.placeholder = "식품을 검색하세요!"
+        searchBar.searchBarStyle = .minimal
+    }
+
+    override func setConstraints() {
+        searchBar.snp.makeConstraints {
+            $0.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(30)
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(100)
+        }
+    }
+}
