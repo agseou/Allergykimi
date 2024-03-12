fdf532c6b423d80b2ef14aba6b275d6a987e5e67
SeoU <tjdnfkd1225@naver.com>
2024-03-12 20:39:30 +0900

    feat: 식약처 정보 기반, 알러지 Enum 생성

---
+++ Allergykimi/Model/Allergy.swift
@@ -0,0 +1,33 @@
+//
+//  Allergy.swift
+//  Allergykimi
+//
+//  Created by 은서우 on 3/12/24.
+//
+
+import Foundation
+
+enum Allergy {
+    case 새우
+    case 굴
+    case 게
+    case 홍합
+    case 오징어
+    case 전복
+    case 고등어
+    case 조개류
+    case 메밀
+    case 밀
+    case 대두
+    case 호두
+    case 땅콩
+    case 잣
+    case 알류
+    case 우유
+    case 쇠고기
+    case 돼지고기
+    case 닭고기
+    case 복숭아
+    case 토마토
+    case 아황산류
+}
